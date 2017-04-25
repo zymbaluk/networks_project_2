917d0dc49ae54700f57ca11d1090b01d92ce696d
Justin Zymbaluk <jzymbalu@uccs.edu>
2017-04-24 19:58:43 -0600

    Finished implementing scaffolding for UDP send_to function, and implemented function to create next packet by reading from file

--- client.c
+++ client.c
@@ -24,16 +24,14 @@
 #define WINDOWGBN 5
 #define WINDOWSAW 1
 
+// globals
+int sequence_number = 0;
+
+//prototypes
+struct gbnpacket next_packet(FILE *file); 
+
 // Usage: 
 int main(int argc, char *argv[]) {
-	int sock;
-	struct sockaddr_in gb_serv_addr;
-	struct sockaddr_in from_addr;
-	unsigned short gbn_serv_port;
-	unsigned int from_size;
-	FILE *file;
-	int window_size;
-
 	//ensure we have the right number of args
 	if (argc != 4) {
 		printf("Usage: %s <file> <gbn|saw> <server-name>\n",
@@ -41,6 +39,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
+	FILE *file;
 	//Attempt to open file, and ensure it's valid
 	file = fopen(argv[1], "rb");
 	if (file == NULL) {
@@ -49,6 +48,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
+	int window_size;
 	//Determine whether we're using GBN or SAW and swet window_size
 	if (strcmp(argv[2],"gbn") != 0 && strcmp(argv[2],"saw") != 0) {
 		printf("Error: neither GBN nor SAW specified\n");
@@ -61,17 +61,73 @@ int main(int argc, char *argv[]) {
 		window_size = WINDOWSAW;
 	}
 
-	struct hostent *hp;
 	//construct our window of size window_size
 	struct gbnpacket window[window_size];
 
-	  if ((hp = gethostbyname(argv[3])) == 0 ) {
-	    printf("Invalid or unknown host\n");
-	    exit(1);
-	  }	
+	struct hostent *hp;
+	//The name of the server we're sending to is given as argv[3], it can be an ip address, or the colloquial name of the server (ie blanca or shavano)
+	if ((hp = gethostbyname(argv[3])) == 0 ) {
+		printf("Invalid or unknown host\n");
+		exit(1);
+	}
+
+	//Create our socket
+	int sk;
+	if ((sk = socket( PF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("Problem creating socket");
+		exit(1);
+	}
+
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	
+	memcpy( &server.sin_addr.s_addr, hp->h_addr, hp->h_length );
+	
+	server.sin_port = htons(PORTNUM);
+
+	
+	struct gbnpacket packet = next_packet(file);
+	printf("contents: %s\n", packet.data);
+	
+/*
+ * 	We should be able to send now through 
+ *
+ * 	n_sent = sendto(sk,buf,buf_len,0,
+ * 	                  (struct sockaddr*) &server,sizeof(server));
+ */
 
 	return 0;
 }
+
+/*
+//Shift our window array over by one
+int shift_by_one(gbnpacket *window, window_size, FILE) {
+	if(size == 1) {
+		//window
+	}
+}
+*/
+struct gbnpacket next_packet(FILE *file) {
+	struct gbnpacket packet;
+	packet.type = 1;
+	packet.sequence_number = sequence_number++;
+	
+	//Now read the data into the packet
+	size_t size = fread( packet.data, sizeof(packet.data[0]), PACKET_SIZE, file);
+	packet.length = size;
+
+	return packet;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- gbnpacket.c
+++ gbnpacket.c
@@ -1,7 +1,14 @@
 //This is the packet which will be sent to and from client and server.
+/*
+ * Type enum:
+ * 	0 - kill packet. show's over
+ * 	1 - normal data packet
+ * 	2 - name packet. This packet is what you should name the file you're about to recieve
+ */
+#define PACKET_SIZE 512
 struct gbnpacket {
 	int type;
 	int sequence_number;
-	int length;
-	char data[512];
+	size_t length;
+	char data[PACKET_SIZE];
 };
